74141e3606d2e4499ecc21dd1b7dd8ea540c82ad
almeidr <rafael.dealmeida@adp.com>
2018-10-12 00:30:24 -0300

    integrantion test implmentation (not complete)

---
+++ src/test/java/com/management/parkingmanagement/integrantion/ParkingManagementIntegrantionTest.java
@@ -0,0 +1,41 @@
+package com.management.parkingmanagement.integrantion;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.ResourceUtils;
+
+import java.io.IOException;
+
+@ActiveProfiles("test")
+@Sql({"/sql/purge.sql", "/sql/seed.sql"})
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class ParkingManagementIntegrantionTest {
+
+    private static final String requestEndpointBase = "http://localhost:8090/fuel-app/api/fuelConsumption";
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    private static String readJSON(String filename) {
+        try {
+            return FileUtils.readFileToString(ResourceUtils.getFile("classpath:" + filename), "UTF-8");
+        } catch (IOException exception) {
+            return null;
+        }
+    }
+
+    private HttpHeaders buildHttpHeaders(){
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        return headers;
+    }
+}
